907d19442267cac3c79c038e873ac9fd370d4613
Famato001 <115541455+Famato001@users.noreply.github.com>
2022-10-31 14:03:34 +0000

    cleaned up plots

--- Plot_acceptance.py
+++ Plot_acceptance.py
@@ -5,13 +5,27 @@ import pandas as pd
 df = pd.read_csv("Data/acceptance_mc.csv")
 df1 = pd.read_csv("Data/total_dataset.csv")
 
-plt.hist(df1['phi'], bins=100)
+
+plt.hist(df['phi'], bins=100, histtype='step',label = 'acceptance.csv',density=True)  
+plt.hist(df1['phi'], bins=100, histtype='step',label='total_dataset.csv',density=True)  
+plt.xlabel('$\phi$')
+plt.legend()
 plt.show()
-plt.hist(df1['costhetal'],bins = 100)
+
+plt.hist(df['costhetak'], bins=100, histtype='step',label = 'acceptance.csv',density=True)  
+plt.hist(df1['costhetak'], bins=100, histtype='step',label='total_dataset.csv',density=True)  
+plt.xlabel('$cos\\theta_{k}$')
+plt.legend()
 plt.show()
-plt.hist(df1['costhetak'],bins=100)
+
+plt.hist(df['costhetal'], bins=100, histtype='step',label = 'acceptance.csv',density=True)  
+plt.hist(df1['costhetal'], bins=100, histtype='step',label='total_dataset.csv',density=True)  
+plt.xlabel('$cos\\theta_{l}$')
+plt.legend()
 plt.show()
-plt.hist(df['q2'], bins=100)
-plt.show()
-plt.hist(df1['q2'], bins=50)
+
+plt.hist(df['q2'], bins=100, histtype='step',label = 'acceptance.csv',density=True)  
+#plt.hist(df1['q2'], bins=100, histtype='step',label='total_dataset.csv',density=True)  
+plt.xlabel('$q^{2}$')
+plt.legend()
 plt.show()
